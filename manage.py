7e7c97b5c5cf50ff1445c1925cd321c559f4d55e
hyuk <testerhyuk214@gmail.com>
2021-07-28 00:16:47 +0900

    update manage.py

--- manage.py
+++ manage.py
@@ -6,8 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hdtsks.settings')
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hdtsks.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hdtsks.settings')
+   
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
