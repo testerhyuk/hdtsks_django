8630ec80742dcb1fd62aa394d57b14e922822327
hyuk <testerhyuk214@gmail.com>
2021-07-27 23:42:59 +0900

    update settings.py

--- hdtsks/settings.py
+++ hdtsks/settings.py
@@ -30,7 +30,7 @@ environ.Env.read_env(
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY=env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
